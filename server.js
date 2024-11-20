4eefb14a578906c9a49f7f577f4b4f629f18a7aa
AngelDel <angelisabroad@gmail.com>
2024-11-20 17:43:51 +0000

    (Remove a couple of debug lines)

--- server.js
+++ server.js
@@ -110,15 +110,12 @@ function setupRoutes() {
     //console.log('Request headers (/profile):', req.headers);    
     console.log('   !T! req.oidc.idToken (/profile):', req.oidc.idToken);  
     console.log('   req.oidc.user (/profile):', req.oidc.user); 
-    console.log('req.oidc.idTokenClaims:', req.oidc.idTokenClaims);
+    //console.log('req.oidc.idTokenClaims:', req.oidc.idTokenClaims);
     console.log('   req.oidc.isAuthenticated (/profile):', req.oidc.isAuthenticated);
     if (req.oidc.isAuthenticated) console.log('      yes'); else console.log('      no');
     
-    console.log('   req.oidc complete object (1)! (/profile):', req.oidc);
-    console.log('   req.oidc complete object (2)! (/profile):', JSON.stringify(req.oidc, null, 2));
-
     const util = require('util');
-    console.log('   req.oidc complete object (3)! (/profile):', util.inspect(req.oidc, { depth: null, showHidden: true }));
+    console.log('   req.oidc complete object! (/profile):', util.inspect(req.oidc, { depth: null, showHidden: true }));
 
     console.log("#");
 
